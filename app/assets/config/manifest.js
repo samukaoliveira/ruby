4ce8722bd1fa63fc52af9695369a7be4c319afc0
samukaoliveira <mucabatera@gmail.com>
2023-11-29 09:59:02 -0300

    pre-configuração do javascript

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,11 @@
 //= link_tree ../builds
 //= link application.bootstrap.css
 //= link rails-ujs.js
+//= link application.js
+
+//= import 'Fileserver.js'
+//= import 'jquery-1.5.2.min.js'
+//= import 'jquery-2.0.1.min.js'
+//= import 'jquery.btechco.excelexport.js'
+//= import 'jquery.maskedinput-1.3.min.js'
+
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,9 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= import 'Fileserver.js'
+//= import 'jquery-1.5.2.min.js'
+//= import 'jquery-2.0.1.min.js'
+//= import 'jquery.btechco.excelexport.js'
+//= import 'jquery.maskedinput-1.3.min.js'
 
